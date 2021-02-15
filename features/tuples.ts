36001dc15759af11a5fb388bdeaaca1983007327
Josh Harris <49429928+jharris711@users.noreply.github.com>
2021-02-14 22:20:21 -0500

    tuples

---
+++ features/tuples.ts
@@ -0,0 +1,16 @@
+const drink = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40
+}
+
+// Type alias:
+type Drink = [string, boolean, number]
+
+/* const pepsi: [string, boolean, number] = ['brown', true, 40]
+pepsi[0] = 40
+pepsi[1] = 'brown' */
+const pepsi: Drink = ['brown', true, 40]
+
+
+const carSpecs: [number, number] = [400, 3354]
